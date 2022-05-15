bfd935620c17c5026cf2c3ea107ec2ec30541e1c
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-15 17:21:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#!/bin/python3
+#!/usr/bin/env python3
+
 import os
 import gi
+
 gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk
 from musicvideos.extras import PublishVideo
 
@@ -14,26 +17,29 @@ class Application:
         self.compress = True
         self.upload = False
 
-    def on_song_input_draw(self, widget, cr):
+    def on_main_window_destroy(self, widget):
+        exit()
+
+    def on_audio_input_draw(self, widget, cr):
         self.song_input_widget = widget
 
-    def on_song_input_insert(self, widget):
+    def on_audio_input_changed(self, widget):
         self.song = widget.get_text()
 
-    def on_song_open_button(self, widget):
+    def on_audio_open_button(self, widget):
         file_chooser = self.builder.get_object('file_chooser_window')
         response = file_chooser.run()
         if response == Gtk.ResponseType.YES:
             file_chooser.hide()
-            self.song = f'"{file_chooser.get_filename()}"'
+            self.song = file_chooser.get_filename().replace(' ', '\ ')
             self.song_input_widget.set_text(self.song)
-        elif response == Gtk.ResponseType.NO:
+        else:
             file_chooser.hide()
 
     def on_cover_input_draw(self, widget, cr):
         self.cover_input_widget = widget
 
-    def on_cover_input_insert(self, widget):
+    def on_cover_input_changed(self, widget):
         self.cover = widget.get_text()
 
     def on_cover_open_button(self, widget):
@@ -41,13 +47,16 @@ class Application:
         response = file_chooser.run()
         if response == Gtk.ResponseType.YES:
             file_chooser.hide()
-            self.cover = f'"{file_chooser.get_filename()}"'
+            self.cover = file_chooser.get_filename().replace(' ', '\ ')
             self.cover_input_widget.set_text(self.cover)
-        elif response == Gtk.ResponseType.NO:
+        else:
             file_chooser.hide()
 
-    def on_artists_input_insert(self, widget):
+    def on_artists_input_changed(self, widget):
         self.artists = widget.get_text()
+
+    def on_song_changed(self, widget):
+        self.name = widget.get_text()
 
     def on_reverb_slide_draw(self, widget, idk):
         self.reverb = widget.get_value()
@@ -57,29 +66,6 @@ class Application:
         self.speed = widget.get_value()
         self.speed_widget = widget
 
-    def on_song_name_insert(self, widget):
-        self.name = widget.get_text()
-
-    def on_build_click(self, widget):
-        original_dir = os.curdir()
-        new_video = PublishVideo()
-        new_video.files(audio=self.song, cover=self.cover)
-        new_video.speed(int(self.speed))
-        new_video.reverb(dry=60, wet=int(self.reverb*10))
-        if ',' in self.artists:
-            self.features = self.artists.split(', ')
-            self.artist = self.features[0]
-            self.features = self.features[1:]
-            new_video.info(artist=self.artist, song=self.name, features=self.features)
-        else:
-            new_video.info(artist=self.artist, song=self.name)
-        if self.compress:
-            new_video.make(compress_files=True)
-        else:
-            new_video.make()
-        if self.upload:
-            new_video.upload(client_secrets=f'{__file__[:-7]}/client_secrets.json')
-        os.chdir(original_dir)
 
     def on_compress_draw(self, widget, cr):
         self.compress_widget = widget
@@ -96,27 +82,53 @@ class Application:
     def on_upload_toggle(self, widget):
         if self.upload:
             self.upload = False
-            return
-
-        warning = self.builder.get_object('warning_client_secrets')
-        response = warning.run()
-        if response == Gtk.ResponseType.YES:
-            warning.hide()
+        else:
             self.upload = True
-            self.upload_widget.set_mode(True)
-        elif response == Gtk.ResponseType.NO:
-            warning.hide()
-            self.upload = False
-            self.upload_widget.set_mode(False)
+
+    
+    def on_yt_input_draw(self, widget, cr):
+        self.yt_input_widget = widget
+
+    def on_yt_input_changed(self, widget):
+        self.client_secrets = widget.get_text()
+
+    def on_yt_open_button(self, widget):
+        file_chooser = self.builder.get_object('file_chooser_window')
+        response = file_chooser.run()
+        if response == Gtk.ResponseType.YES:
+            file_chooser.hide()
+            self.client_secrets = file_chooser.get_filename().replace(' ', '\ ')
+            self.yt_input_widget.set_text(self.client_secrets)
+        else:
+            file_chooser.hide()
+
 
     def on_slowed_default_click(self, widget):
-        self.reverb_widget.set_value(2.5)
+        self.reverb_widget.set_value(2.0)
         self.speed_widget.set_value(-4.0)
 
     def on_nightcore_default_click(self, widget):
-        self.reverb_widget.set_value(2.5)
+        self.reverb_widget.set_value(2.0)
         self.speed_widget.set_value(5.0)
 
+    def on_build_click(self, widget):
+        new_video = PublishVideo()
+        new_video.files(audio=self.song, cover=self.cover)
+        new_video.speed(int(self.speed))
+        new_video.reverb(dry=60, wet=int(self.reverb*10))
+        if ',' in self.artists:
+            self.features = self.artists.split(', ')
+            self.artist = self.features[0]
+            self.features = self.features[1:]
+            new_video.info(artist=self.artist, song=self.name, features=self.features)
+        else:
+            new_video.info(artist=self.artists, song=self.name)
+        if self.compress:
+            new_video.make(compress_files=True)
+        else:
+            new_video.make()
+        if self.upload:
+            new_video.upload(client_secrets=self.client_secrets)    
 
 Application()
 Gtk.main()
