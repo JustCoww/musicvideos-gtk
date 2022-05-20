3fcb657778ced823a6206d4852e347d1ba014194
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-20 17:45:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ pip install --upgrade pygobject;
 sleep 1s
 clear
 echo "Downloading..."
-sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/bin/musicvideos-gtk" -o "/usr/bin/musicvideos-gtk" > /dev/null;
+sudo curl -H "Cache-Control: no-cache" "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/bin/musicvideos-gtk" -o "/usr/bin/musicvideos-gtk" > /dev/null;
 sudo mkdir "/usr/share/musicvideos-gtk/"
-sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/icons/icon.svg" -o "/usr/share/musicvideos-gtk/icon.svg" > /dev/null;
-sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/musicvideos-gtk.desktop" -o "/usr/share/applications/org.justcow.musicvideos-gtk.desktop"
+sudo curl -H "Cache-Control: no-cache" "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/icons/icon.svg" -o "/usr/share/musicvideos-gtk/icon.svg" > /dev/null;
+sudo curl -H "Cache-Control: no-cache" "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/musicvideos-gtk.desktop" -o "/usr/share/applications/org.justcow.musicvideos-gtk.desktop"
 sudo chmod +x "/usr/bin/musicvideos-gtk" ;
 
 echo "Done!"
