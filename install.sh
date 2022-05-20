a4da2d88fed336f9fa3f4e9baca5eded191a29a9
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-20 13:52:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/sh
-echo "Installing..."
-pip3 install --upgrade musicvideos
-chmod +x bin/*
-sudo cp bin/* /usr/bin/
+echo "Installing musicvideos pip package"
+pip install --upgrade musicvideos > /dev/null ;
+sleep 1s
+echo "Downloading..."
+sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/bin/musicvideos-gtk" -o "/usr/bin/musicvideos-gtk" > /dev/null;
+sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/icons/icon.svg" -o "/usr/share/musicvideos-gtk/icon.svg" > /dev/null;
+sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/musicvideos-gtk.desktop" -o "/usr/share/applications/org.justcow.musicvideos-gtk.desktop"
+sudo chmod +x "/usr/bin/musicvideos-gtk" ;
+
 echo "Done!"
