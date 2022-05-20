6a12a47296b2be1f12fc98014bf3d72def15a565
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-20 13:59:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ pip install --upgrade pygobject > /dev/null ;
 sleep 1s
 echo "Downloading..."
 sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/bin/musicvideos-gtk" -o "/usr/bin/musicvideos-gtk" > /dev/null;
+sudo mkidr "/usr/share/musicvideos-gtk/"
 sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/icons/icon.svg" -o "/usr/share/musicvideos-gtk/icon.svg" > /dev/null;
 sudo curl "https://raw.githubusercontent.com/JustCoww/musicvideos-gtk/main/musicvideos-gtk.desktop" -o "/usr/share/applications/org.justcow.musicvideos-gtk.desktop"
 sudo chmod +x "/usr/bin/musicvideos-gtk" ;
